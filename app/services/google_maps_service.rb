0191857d6a1be7e3c0b18484420d85e5a5442621
BruceNotLee <bruce.lesko@outlook.com>
2018-02-10 18:38:00 -0500

    Moving over to private github

---
+++ app/services/google_maps_service.rb
@@ -0,0 +1,31 @@
+class GoogleMapsService
+  @@google_key = ENV['google_key']
+  class << self
+    def root_path
+      'https://maps.googleapis.com/maps/api/'
+    end
+
+    def nearby_places(zip)
+      url_path = root_path + 'place/nearbysearch/json?'
+
+      params ="location=#{geo_location(zip)}&radius=16093&type=restaurant&key=#{@@google_key}"
+
+      response = RestClient.get(url_path+params)
+
+      response_body = JSON.parse(response.body)
+
+      response_body['results']
+    end
+
+    def geo_location(zip)
+      url_path = root_path + 'geocode/json?'
+
+      response = RestClient.get(url_path + "components=postal_code:#{zip}")
+      response_body = JSON.parse(response.body)
+
+      location = response_body['results'][0]['geometry']['location']
+
+      "#{location['lat']},#{location['lng']}"
+    end
+  end
+end
