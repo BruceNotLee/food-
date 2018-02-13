9ee7443ea74fbf34c0ca614eacbaab91a7479597
BruceNotLee <bruce.lesko@outlook.com>
2018-02-13 17:56:08 -0500

    working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,28 +1,22 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index, :address_search]
   def index
-    @meals = search_results = Unsplash::Photo.search("food").sample["urls"]["small"]
-    # p "<>"*30
-    # p "in controller"
-    # unless params[:search].nil?
-    #   p "in unless"
-    #   p params[:search]
-    #   @places = GoogleMapsService.nearby_places params[:search]
-    #   @local_resturants = @places.map do |r|
-    #     pd = GoogleMapsService.place_details(r['place_id'])['result']
-    #     {
-    #       id: r['place_id'],
-    #       name: r['name'],
-    #       opening_hours: r['opening_hours'],
-    #       details: {
-    #         types: pd['types'],
-    #         vicinity: pd['vicinity']
-    #       }
-    #     }
-    #   end
-    #
-    # end
-
+    unless params[:search].nil?
+      p params[:search]
+      @places = GoogleMapsService.nearby_places params[:search]
+      @local_resturants = @places.map do |r|
+        pd = GoogleMapsService.place_details(r['place_id'])['result']
+        {
+          id: r['place_id'],
+          name: r['name'],
+          opening_hours: r['opening_hours'],
+          details: {
+            types: pd['types'],
+            vicinity: pd['vicinity']
+          }
+        }
+      end
+    end
     Rails.logger.info @places
   end
 
