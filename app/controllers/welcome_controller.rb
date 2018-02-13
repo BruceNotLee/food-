4598c6918430fbf91c509c46c46e10eba9d65701
BruceNotLee <bruce.lesko@outlook.com>
2018-02-13 10:08:46 -0500

    Google zipcode search same page results

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,20 +1,33 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index, :address_search]
   def index
-    @places = GoogleMapsService.nearby_places 33131
-    Rails.logger.info @places
+    p "<>"*30
+    p "in controller"
+    unless params[:search].nil?
+      p "in unless"
+      p params[:search]
+      @places = GoogleMapsService.nearby_places params[:search]
+      @local_resturants = @places.map do |r|
+        pd = GoogleMapsService.place_details(r['place_id'])['result']
+        {
+          id: r['place_id'],
+          name: r['name'],
+          opening_hours: r['opening_hours'],
+          details: {
+            types: pd['types'],
+            vicinity: pd['vicinity']
+          }
+        }
+      end
 
+    end
+
+    Rails.logger.info @places
   end
 
   def show
-    @places = GoogleMapsService.nearby_places 33131
+    @places = GoogleMapsService.nearby_places
     Rails.logger.info @places
-
-  end
-
-  def address_search
-    @response = GoogleMapsService.nearby_places(params[:search])
-    render 'maps/index', response: @response
   end
 
 end
