9b322ec83aba39babe8a8c31e2f74cc8636ffd14
Shoshana Reuveni <kitten81383@yahoo.com>
2018-03-04 15:20:03 -0500

    desktop home page

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,6 +1,6 @@
 Unsplash.configure do |config|
-  config.application_id     = "e0017b1d5a5c0283687202de4cf9460dd621209abe7e1594452a5b81a04f3b4e"
-  config.application_secret = "fd37001c29552a52fd657c3ee65fdb93d75f1cc03aa67c964482ddced3a0c9e1"
+  config.application_id     = "391bb3f53c95075284685bea644f37fa19fa1b3b9d48ab0cdbf86d6218d38161"
+  config.application_secret = "b4c23b1c666ce691bd6e2d6019a406bfa22d5e994f9036b26ecb440db37a1052"
   config.application_redirect_uri = "http://localhost:3000"
   config.utm_source = "no_spaces_in_the_name"
 end
